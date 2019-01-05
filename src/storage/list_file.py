45667faa2d876f09a3ba24f4a693a505a620fe36
lrm25 <luke.r.mccrone@gmail.com>
2019-01-04 22:18:56 -0700

    Added tests for list file object

--- src/storage/list_file.py
+++ src/storage/list_file.py
@@ -13,6 +13,18 @@ class ListFile:
     # file_name:  full or relative path of list file
     #
     def __init__(self, file_name):
+
+        if file_name is None or file_name == "":
+            raise ValueError("Empty or no file name for list file")
+
+        name, extension = os.path.splitext(file_name)
+        if extension != ".txt":
+            raise ValueError("File %s is not a .txt file" % file_name)
+
+        if not os.path.isfile(file_name):
+            raise IOError("File %s doesn't exist or is not a regular file" % 
+                          file_name)
+
         self._file_name = file_name
 
     # 
@@ -22,16 +34,14 @@ class ListFile:
         return self._file_name
 
     # 
-    # TODO move to main
+    # TODO move up to master_list
     # return:  get all lines from this file
     #
     def get_lines(self):
         lines = []
-        line_num = 1
         basename = os.path.basename(self._file_name)
         with open(self._file_name) as f:
-            for line in f.readlines():
+            for line_num, line in enumerate(f.readlines(), 1):
                 stripped_line = line.rstrip()
                 lines.append((basename, line_num, stripped_line))
-                line_num = line_num + 1
         return lines
---
+++ test/README.md
@@ -0,0 +1,3 @@
+To run tests, set $PYTHONPATH to 
+'''<repository base>/src'''
+Then just run "pytest" in the test folder.
